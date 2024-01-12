c6c59c1dd19109be6ee66acd8c9ff0ae6533e75c
cfultz <caleb@fultz.dev>
2024-01-12 12:20:22 -0500

    Added Main and Bullet scenes. Ship shoots and wraps.

--- Space Rocks/scenes/player/player.gd
+++ Space Rocks/scenes/player/player.gd
@@ -3,15 +3,20 @@ extends RigidBody2D
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 @export var engine_power = 500
 @export var spin_power = 8000
+@export var bullet_scene : PackedScene
+@export var fire_rate = 0.25
+
+
 var state = INIT
 var thrust = Vector2.ZERO
 var rotation_dir = 0
 var screensize = Vector2.ZERO
-
+var can_shoot = true
 
 func _ready():
 	change_state(ALIVE)
 	screensize = get_viewport_rect().size
+	$GunCooldown.wait_time = fire_rate
 
 func change_state(new_state):
 	match new_state:
@@ -35,6 +40,8 @@ func get_input():
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
 	
 func _physics_process(delta):
 	constant_force = thrust
@@ -45,3 +52,16 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+
+func shoot():
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
