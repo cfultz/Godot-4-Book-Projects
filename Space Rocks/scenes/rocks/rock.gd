b4968442dc654513772dfe8da02834d3bd155e2c
cfultz <caleb@fultz.dev>
2024-01-14 13:53:19 -0500

    Rocks now explode when shot by laser

--- Space Rocks/scenes/rocks/rock.gd
+++ Space Rocks/scenes/rocks/rock.gd
@@ -5,6 +5,8 @@ var size
 var radius
 var scale_factor = 0.2
 
+signal exploded
+
 func start(_position, _velocity, _size):
 	position = _position
 	size = _size
@@ -16,9 +18,21 @@ func start(_position, _velocity, _size):
 	$CollisionShape2D.shape = shape
 	linear_velocity = _velocity
 	angular_velocity = randf_range(-PI, PI)
+	$Explosion.scale = Vector2.ONE * 0.75 * size
 	
 func _integrate_forces(physics_state):
 	var xform = physics_state.transform
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+	
+func explode():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
